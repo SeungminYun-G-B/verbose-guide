cbb901e9c381070ac71a66c858b9bf271e990cb6
SeungminYun-G-B <yunseungmin6972@naver.com>
2023-10-17 15:09:53 +0900

    p5js_change

--- sketch.js
+++ sketch.js
@@ -1,48 +1,18 @@
-// let mover;
-let balls=[];
+let ball;
 
 function setup() {
-  createCanvas(400, 400);
-  // mover = new Mover();
-  for(let i = 0; i<20; i++){
-    b = new Mover();
-    balls.push(b);
-  }
-  
+  createCanvas(400, 600);
+
+  ball = new Particle();
 }
+
 
 function draw() {
   background(220);
-  for(let b of balls){
-  for(let i = 0; i<10; i++){
-  b.update();
-  b.display();
-  }
-  }
-}
 
+  let gravity = createVector(0, 0.3);
+  ball.addForce(gravity);
 
-
-class Mover{
-  constructor(){
-    this.pos = createVector(random(width), random(height));
-    this.acc = createVector();
-    this.vel = createVector();
-  }
-  
-  update(){
-    let mouse = createVector(mouseX,mouseY);
-    let dir = p5.Vector.sub(mouse,this.pos);
-    dir.setMag(0.2);
-    
-    this.acc=dir;
-    this.vel.add(this.acc);
-    this.vel.limit(4);
-    this.pos.add(this.vel);
-  }
-  
-  display(){
-    noStroke();
-    circle(this.pos.x, this.pos.y, 5);
-  }
+  ball.update();
+  ball.show();
 }
